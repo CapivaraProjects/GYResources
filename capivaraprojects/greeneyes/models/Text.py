75c18b80df8b68861ab8213032cee6fe778af02a
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-01-13 17:54:32 -0200

    fix tabbed

--- capivaraprojects/greeneyes/models/Text.py
+++ capivaraprojects/greeneyes/models/Text.py
@@ -2,7 +2,7 @@ class Text:
 
 	def __init__(self, id=0, language="", tag="", value="", description=""):
 		self.id = id
-        self.language = language
-        self.tag = tag
-        self.value = value
-        self.description = description
+        	self.language = language
+       		self.tag = tag
+        	self.value = value
+        	self.description = description
