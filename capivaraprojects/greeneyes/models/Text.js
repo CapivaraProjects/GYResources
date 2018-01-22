508c1bfbdddbd2e3124a42b89923224cf891cb02
WendelHime <wendelhime@hotmail.com>
2018-01-22 20:43:43 -0200

    add models as submodule

--- capivaraprojects/greeneyes/models/Text.js
+++
@@ -1,9 +0,0 @@
-class Text {
-  constructor(id=0, language="", tag="", value="", description="") {
-    this.id = id;
-    this.language = language;
-    this.tag = tag;
-    this.value = value;
-    this.description = description;
-  }
-}
--- capivaraprojects/greeneyes/models/Type.js
+++
@@ -1,7 +0,0 @@
-class Type {
-	constructor (id = 0, value = "", description = "") {
-		this.id = id;
-		this.value = value;
-		this.description = description;
-	}
-}
