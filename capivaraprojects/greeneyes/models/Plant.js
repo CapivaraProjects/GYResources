665c949500ee33b60889d82e908fb7246ec3107a
Wendel Hime <wendelhime@hotmail.com>
2018-01-22 00:15:01 -0200

    Revert "Create capivaraprojects.greeneyes.models in JS, User model in JS and Python and Type model in Python"

--- capivaraprojects/greeneyes/models/Plant.js
+++
@@ -1,8 +0,0 @@
-class Plant {
-	constructor (id = 0, scientificName = "", commonName = "", diseases = list()) {
-		this.id = id;
-		this.scientificName = scientificName;
-		this.commonName = commonName;
-		this.diseases = diseases;
-	}
-}
