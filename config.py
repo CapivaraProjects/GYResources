6babac1490a3e30565ef31f25acded63add0f301
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-02-04 16:24:50 -0200

    add settings file

---
+++ config.py
@@ -0,0 +1,17 @@
+class DefaultConfig(object):
+    # Database setting
+    DBHOST = '127.0.0.1'
+    DBUSER = 'capivara'
+    DBPASS = 'test'
+    DBPORT = '5432'
+    DBNAME = 'green_eyes'
+    # Application setting
+    SERVER_NAME = '127.0.0.1:8888'
+    SWAGGER_UI_DOC_EXPANSION = 'list'
+    RESTPLUS_VALIDATE = True
+    RESTPLUS_MASK_SWAGGER = False
+    RESTPLUS_ERROR_404_HELP = False
+
+class TestConfig(object):
+    # test settings
+    pass
