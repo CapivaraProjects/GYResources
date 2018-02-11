43a3ba7966369878fd8107fc8a0c84a7d47a7eea
WendelHime <wendelhime@hotmail.com>
2018-02-11 02:23:09 -0200

    auth plant

--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,9 +1,11 @@
 import json
 import pytest
+import base64
+import models.Plant
 from flask import Flask
 from app import initialize_app
 from collections import namedtuple
-import models.Plant
+from tools.Cryptography import Crypto
 
 
 app = Flask(__name__)
@@ -12,6 +14,56 @@ client = app.test_client
 generic_plant = models.Plant.Plant(
         scientificName='test',
         commonName='test')
+
+generic_user = models.User.User(
+        idType=1,
+        email='test@test.com',
+        username='test',
+        password='test',
+        salt='test',
+        dateInsertion='03/02/2018',
+        dateUpdate='10/02/2018')
+
+
+def auth(generic_user=generic_user):
+    crypto = Crypto()
+    generic_user.salt = crypto.generateRandomSalt()
+    generic_user.password = crypto.encrypt(
+        generic_user.salt,
+        'test')
+
+    data = {'salt': generic_user.salt}
+    creds = base64.b64encode(
+        bytes(
+            generic_user.username+":"+generic_user.password,
+            'utf-8')).decode('utf-8')
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Basic %s' % creds
+    }
+    resp = client().post(
+        '/api/gyresources/token/',
+        headers=headers,
+        data=str(
+            json.dumps(data)),
+        follow_redirects=True)
+    resp = json.loads(resp.get_data(as_text=True))
+    token = resp['response']
+    generic_user.password = 'password'
+    return (generic_user, token)
+
+
+generic_user = models.User.User(
+    id=generic_user.id,
+    idType=generic_user.idType,
+    email=generic_user.email,
+    username=generic_user.username,
+    password='test',
+    salt=generic_user.salt,
+    dateInsertion=generic_user.dateInsertion,
+    dateUpdate=generic_user.dateUpdate)
+(generic_user, token) = auth(generic_user)
 
 
 @pytest.mark.order1
@@ -76,10 +128,13 @@ def test_search():
 @pytest.mark.order4
 def test_create(generic_plant=generic_plant):
     data = generic_plant.__dict__
-    resp = client().post('/api/gyresources/plants/', data=str(
-        json.dumps(generic_plant.__dict__)), headers={
+    headers = {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'})
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer %s' % token['token']
+            }
+    resp = client().post('/api/gyresources/plants/', data=str(
+        json.dumps(data)), headers=headers)
     plant = json.loads(
                 resp.get_data(as_text=True))['response']
     plant = namedtuple("Plant", plant.keys())(*plant.values())
@@ -112,10 +167,13 @@ def test_update(generic_plant=generic_plant):
                 "commonName": plant.commonName
             }
     generic_plant.scientificName = 'update'
-    resp = client().put('/api/gyresources/plants/', data=str(
-        json.dumps(plant)), headers={
+    headers = {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'})
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer %s' % token['token']
+            }
+    resp = client().put('/api/gyresources/plants/', data=str(
+        json.dumps(plant)), headers=headers)
     assert resp.status_code == 200
     plant = json.loads(
                 resp.get_data(as_text=True))
@@ -146,10 +204,13 @@ def test_delete(generic_plant=generic_plant):
                 "scientificName": plant.scientificName,
                 "commonName": plant.commonName
             }
-    resp = client().delete('/api/gyresources/plants/', data=str(
-        json.dumps(plant)), headers={
+    headers = {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'})
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer %s' % token['token']
+            }
+    resp = client().delete('/api/gyresources/plants/', data=str(
+        json.dumps(plant)), headers=headers)
     assert resp.status_code == 200
     assert 204 == json.loads(
             resp.get_data(as_text=True))['status_code']
