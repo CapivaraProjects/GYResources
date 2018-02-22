3b74ba3d0669699107d6ae7df11566f5d7483783
WendelHime <wendelhime@hotmail.com>
2018-02-21 23:27:25 -0300

    update config

--- api/restplus.py
+++ api/restplus.py
@@ -10,92 +10,101 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
 
 api = Api(version='1.0', title='GYResources-API',
           description='Services application used by Green Eyes applications')
-flask_app = Flask(__name__)
-flask_app.config.from_object('config.DefaultConfig')
-flask_app.config['SECRET_KEY'] = 'my secret'
+FLASK_APP = Flask(__name__)
+FLASK_APP.config.from_object('config.TestConfig')
 auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth('Bearer')
 multi_auth = MultiAuth(auth, token_auth)
 
 
-def generate_auth_token(expiration=600, id=0):
-    s = Serializer(flask_app.config['SECRET_KEY'], expires_in=expiration)
-    Logger.Logger.create(flask_app.config["ELASTICURL"],
+def generate_auth_token(expiration=FLASK_APP.config['EXPIRATION_TOKEN'], user_id=0):
+    """(int, int) -> (token)
+    Method used to generate auth token
+    """
+    serializer = Serializer(FLASK_APP.config['SECRET_KEY'], expires_in=expiration)
+    Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                          'Informative',
                          'Ok',
                          'generate_auth_token()',
-                         str(id),
-                         flask_app.config["TYPE"])
-    return s.dumps({'id': id})
+                         str(user_id),
+                         FLASK_APP.config["TYPE"])
+    return serializer.dumps({'id': id})
 
 
 def verify_auth_token(token):
-    s = Serializer(flask_app.config['SECRET_KEY'])
+    """(token) -> (User)
+    Method used to verify auth token
+    """
+    serializer = Serializer(FLASK_APP.config['SECRET_KEY'])
     try:
-        data = s.loads(token)
+        data = serializer.loads(token)
     except SignatureExpired:
         # valid token but expired
-        Logger.Logger.create(flask_app.config["ELASTICURL"],
+        Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                              'Error',
                              'Valid token but expired',
                              'verify_auth_token()',
                              str(token),
-                             flask_app.config["TYPE"])
+                             FLASK_APP.config["TYPE"])
         return None
     except BadSignature:
-        Logger.Logger.create(flask_app.config["ELASTICURL"],
+        Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                              'Error',
                              'Invalid token',
                              'verify_auth_token()',
                              str(token),
-                             flask_app.config["TYPE"])
+                             FLASK_APP.config["TYPE"])
         # invalid token
         return None
 
     repository = UserRepository(
-                flask_app.config["DBUSER"],
-                flask_app.config["DBPASS"],
-                flask_app.config["DBHOST"],
-                flask_app.config["DBPORT"],
-                flask_app.config["DBNAME"])
+        FLASK_APP.config["DBUSER"],
+        FLASK_APP.config["DBPASS"],
+        FLASK_APP.config["DBHOST"],
+        FLASK_APP.config["DBPORT"],
+        FLASK_APP.config["DBNAME"])
     user = repository.searchByID(data['id'])
-    Logger.Logger.create(flask_app.config["ELASTICURL"],
+    Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                          'Informative',
                          'User logged',
                          'verify_auth_token()',
                          str(user.__dict__),
-                         flask_app.config["TYPE"])
+                         FLASK_APP.config["TYPE"])
     return user
 
 
 @token_auth.verify_token
 def verify_token(token):
+    """(token) -> (bool)
+    Method used to verify token
+    """
     g.user = verify_auth_token(token)
     if g.user is not None:
-        Logger.Logger.create(flask_app.config["ELASTICURL"],
-                         'Informative',
-                         'Token verified',
-                         'verify_token()',
-                         str(g.user.__dict__),
-                         flask_app.config["TYPE"])
+        Logger.Logger.create(
+            FLASK_APP.config["ELASTICURL"],
+            'Informative',
+            'Token verified',
+            'verify_token()',
+            str(g.user.__dict__),
+            FLASK_APP.config["TYPE"])
     else:
-        Logger.Logger.create(flask_app.config["ELASTICURL"],
+        Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                              'Informative',
                              'User not defined',
                              'verify_token()',
                              str(token),
-                             flask_app.config["TYPE"])
-    return (g.user is not None)
+                             FLASK_APP.config["TYPE"])
+    return g.user is not None
 
 
 @auth.verify_password
 def verify_password(usernameOrToken, password):
     repository = UserRepository(
-            flask_app.config["DBUSER"],
-            flask_app.config["DBPASS"],
-            flask_app.config["DBHOST"],
-            flask_app.config["DBPORT"],
-            flask_app.config["DBNAME"])
+            FLASK_APP.config["DBUSER"],
+            FLASK_APP.config["DBPASS"],
+            FLASK_APP.config["DBHOST"],
+            FLASK_APP.config["DBPORT"],
+            FLASK_APP.config["DBNAME"])
     user = models.User.User(
             username=usernameOrToken,
             password=password,
@@ -104,20 +113,20 @@ def verify_password(usernameOrToken, password):
         user = repository.authentication(user)
         if (user.id):
             g.user = user
-            Logger.Logger.create(flask_app.config["ELASTICURL"],
+            Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                                  'Informative',
                                  'Authenticated',
                                  'verify_password()',
                                  str(user.__dict__),
-                                 flask_app.config["TYPE"])
+                                 FLASK_APP.config["TYPE"])
             return True
     except Exception as err:
-        Logger.Logger.create(flask_app.config["ELASTICURL"],
+        Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                              'Error',
                              'Failed to authenticate',
                              'verify_password()',
                              str(err),
-                             flask_app.config["TYPE"])
+                             FLASK_APP.config["TYPE"])
         return False
 
 
@@ -127,22 +136,22 @@ def unauthorized():
         'status_code': 401,
         'error': 'unauthorized',
         'message': 'Please authenticate to access this API.'})
-    Logger.Logger.create(flask_app.config["ELASTICURL"],
+    Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                          'Error',
                          'Unauthorized',
                          'unauthorized()',
                          str(response.__dict__),
-                         flask_app.config["TYPE"])
+                         FLASK_APP.config["TYPE"])
     return response
 
 
 @api.errorhandler
 def default_error_handler(e):
     message = 'An unhandled exception occurred.'
-    Logger.Logger.create(flask_app.config["ELASTICURL"],
+    Logger.Logger.create(FLASK_APP.config["ELASTICURL"],
                          'Error',
                          'Internal server error',
                          'default_error_handler()',
                          message,
-                         flask_app.config["TYPE"])
+                         FLASK_APP.config["TYPE"])
     return {'message': message}, 500
