8cee3630a3f120849cde220d00f38ba0be605c80
WendelHime <wendelhime@hotmail.com>
2018-09-01 12:28:56 -0300

    print error

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -79,6 +79,7 @@ def test_create(generic_analysis=generic_analysis, generic_user=generic_user):
             }
     resp = client().post('/api/gyresources/analysis/', data=str(
         json.dumps(data)), headers=headers)
+    print(resp.get_data(as_text=True))
     analysis = json.loads(resp.get_data(as_text=True))['response']
     print(analysis)
     response = namedtuple("Response", analysis.keys())(*analysis.values())
